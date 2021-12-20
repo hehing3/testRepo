a36d7329eb2e667aa056b77c76a46624efc274c9
ALL4LAND <ALL4LAND@host.docker.internal>
2021-12-20 15:41:43 +0900

    345345345

---
+++ testProject/src/testProject/test.java
@@ -0,0 +1,12 @@
+package testProject;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String ss = "Aaaa";
+		System.out.println("aaaaaaaa");
+		
+	}
+
+}
