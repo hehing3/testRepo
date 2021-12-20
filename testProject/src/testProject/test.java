1ac8deb6bdd00ab44ec739f5383a53e28e3de91b
ALL4LAND <ALL4LAND@host.docker.internal>
2021-12-20 16:15:42 +0900

    테스트2021

--- testProject/src/testProject/test.java
+++ testProject/src/testProject/test.java
@@ -5,6 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String ss = "Aaaa1";
+		String sscc = "Aaaa12";
 		System.out.println("aaaaaaaa222"); 
 	}
 
